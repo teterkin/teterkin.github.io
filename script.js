4e28d0d93dd4cb4dc915462e6c47a8c15e732917
Alexander Teterkin <amteterkin@yandex.ru>
2026-07-16 23:57:29 +0300

    Add Blog link with animated appearance - displays after Welcome! with 1s delay to blog.teterkin.ru

--- script.js
+++ script.js
@@ -4,18 +4,3 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
-
-// Ждем полной загрузки страницы
-window.addEventListener('DOMContentLoaded', () => {
-    const welcomeTitle = document.querySelector('.welcome');
-    const blogBlock = document.getElementById('blog-animation-block');
-
-    if (welcomeTitle && blogBlock) {
-        // Слушаем событие окончания CSS-анимации у надписи Welcome!
-        welcomeTitle.addEventListener('animationend', () => {
-            // Как только Welcome появилась, активируем появление ссылки Blog
-            blogBlock.classList.add('blog-visible');
-        });
-    }
-});
-
